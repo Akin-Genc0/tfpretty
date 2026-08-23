4afb3ea266d362a9b987fd7aec8d495263fce873
Akin-Genc0 <akingenc212@gmail.com>
2026-08-23 13:58:07 +0100

    feat: add CLI entry point

---
+++ cmd/root.go
@@ -0,0 +1,6 @@
+
+var rootCmd = &cobra.Command{
+    Use:   "tfpretty",
+    Short: "A beautiful Terraform plan viewer",
+    Long:  "tfpretty makes Terraform plans easier to read directly from your terminal.",
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Akin-Genc0/tfpretty/cmd"
+
+func main() {
+	cmd.Execute()
+}
