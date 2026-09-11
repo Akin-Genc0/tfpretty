3587175bb60372840d9d152afd82b9252b31ab72
Akin-Genc0 <akingenc212@gmail.com>
2026-09-11 16:45:42 +0100

    feat(terraform): parse plan JSON into typed structs
    Unmarshal Terraform plan JSON into the existing Plan type and return descriptive parsing errors.

--- internal/terraform/parser.go
+++ internal/terraform/parser.go
@@ -1,1 +1,17 @@
 package terraform
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func ParsePlan(data []byte) (Plan, error) {
+
+	var plan Plan
+
+	err := json.Unmarshal(data, &plan)
+	if err != nil {
+		return Plan{}, fmt.Errorf("could not parse Terraform plan JSON: %w", err)
+	}
+	return plan, nil
+}
